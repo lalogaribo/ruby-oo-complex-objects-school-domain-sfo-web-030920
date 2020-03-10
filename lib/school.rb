c5910c42b16b94a7e0f92575f281f79ffe21ff33
Eduardo mendoza <garibo.eduardo90@gmail.com>
2020-03-10 23:30:42 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require 'pry'
+class School
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    @roster.has_key?(grade) ? @roster[grade] << student_name : @roster[grade] = [student_name]
+  end
+
+  def roster
+    @roster
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    new_roster = @roster.sort_by { |k, v| k}
+    sorted_roster = {}
+    new_roster.each do |k, v|
+      sorted_roster[k] = v.sort
+    end
+    sorted_roster.to_h
+  end
+end
