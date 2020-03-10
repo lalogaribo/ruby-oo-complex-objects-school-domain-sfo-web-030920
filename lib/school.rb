0828df688ba79c20873ea8752dfec39249c9e949
Eduardo mendoza <garibo.eduardo90@gmail.com>
2020-03-10 21:56:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    @roster.has_key?(grade) ? @roster[grade] << student_name : @roster[grade] = [student_name]
+  end
+
+  def roster
+    @roster
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    @roster.sort
+  end
+end
